dfe1152259e683f525018728ff5e2d4cb134c0a0
Jason Huang <jason5528460@gmail.com>
2021-01-21 03:55:41 +0800

    shift animation

--- src/frontend/component/shift.js
+++ src/frontend/component/shift.js
@@ -4,8 +4,11 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography'
 import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
 import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { Container } from '@material-ui/core';
 
 const useStyles = makeStyles({
@@ -15,7 +18,7 @@ const useStyles = makeStyles({
 	header: {
 		marginTop: '16px',
 		display: 'flex',
-		width: '70%',
+		width: '600px',
 		margin: '0 auto',
 		marginBottom: '30px',
 	},
@@ -28,7 +31,7 @@ const useStyles = makeStyles({
 	},
 	paper: {
 		display: 'flex',
-		width: '70%',
+		width: '600px',
 		margin: '0 auto',
 		marginTop: '20px',
 		marginBottom: '10px',
@@ -43,9 +46,49 @@ const useStyles = makeStyles({
 		marginBottom: '10px',
 	},
 	seatPaper: {
-		margin: '0 auto',
-		width: '70%',
+		width: '500px',
 		height: '150px',
+		scrollBehavior: 'smooth',
+		overflowX: 'auto',
+		overflowY: 'hidden',
+		whiteSpace: 'nowrap',
+	},
+	button: {
+
+	},
+	carContainer: {
+		width: '2000px',
+		display: 'flex',
+	},
+	car: {
+		width: '500px',
+		padding: '0 55px',
+		margin: '11px auto',
+		display: 'flex',
+		flexWrap: 'wrap',
+	}, 
+	seat: {
+		height: '25px',
+		width: '25px',
+		margin: '3px 6px 3px 6px',
+		backgroundColor: 'rgb(180,180,180)',
+		borderRadius: '3px',
+		'&:hover': {
+			backgroundColor: 'rgb(210,210,210)',
+			cursor: 'pointer',
+		}
+	},
+	seatDisable: {
+		height: '25px',
+		width: '25px',
+		margin: '3px 6px 3px 6px',
+		backgroundColor: 'rgb(240,240,240)',
+		borderRadius: '3px',
+	}, 
+	bigContainer: {
+		display: 'flex',
+		margin: '0 auto',
+		width: '600px',
 	}
 });
 
@@ -56,7 +99,8 @@ function createData(outdata) {
   const arrival = outdata.arrival
   const departtime = outdata.schedule.depart
   const arrivetime = outdata.schedule.arrive
-  return { trainNum, date, departure, departtime, arrival, arrivetime };
+	const seats = outdata.seats
+  return { trainNum, date, departure, departtime, arrival, arrivetime, seats };
 }
 
 // let rows = [{trainNum: 1, date: {year: 2020, month: 12, day: 12}, departure: "a", arrival: "b", departtime: { hour: 10, minute: 10 }, arrivetime: { hour: 11, minute: 10 } }, {trainNum: 1, date: {year: 2020, month: 12, day: 12}, departure: "a", arrival: "b", departtime: { hour: 10, minute: 10 }, arrivetime: { hour: 11, minute: 10 } }];
@@ -66,6 +110,11 @@ export default function Shift(props) {
   const classes = useStyles();
   const { shiftData, departure, dest } = props;
 	const [seatOpened, setSeatOpened] = useState(new Array(5).fill(false));
+	const [currentIdx, setCurrentIdx] = useState(0);
+	const ref0 = useRef(null);
+	const ref1 = useRef(null);
+	const ref2 = useRef(null);
+	const ref3 = useRef(null);
 
   useEffect(() => {
 		rows = [];
@@ -82,6 +131,18 @@ export default function Shift(props) {
 		arr[idx] = !(arr[idx]);
 		setSeatOpened(arr);
 	}
+	const handlePreviousClick = () => {
+		if ( currentIdx === 1 ) ref0.current.scrollIntoView();
+		else if ( currentIdx === 2 ) ref1.current.scrollIntoView();
+		else if ( currentIdx === 3 ) ref2.current.scrollIntoView();
+		setCurrentIdx(currentIdx-1);
+	}
+	const handleNextClick = () => {
+		if ( currentIdx === 0 ) ref1.current.scrollIntoView();
+		else if ( currentIdx === 1 ) ref2.current.scrollIntoView();
+		else if ( currentIdx === 2 ) ref3.current.scrollIntoView();
+		setCurrentIdx(currentIdx+1);
+	}
 
   return (
 		<Container className={classes.root}>
@@ -96,6 +157,7 @@ export default function Shift(props) {
 					const { trainNum, departtime, arrivetime, seats } = val;
 					const { hour: d_hour, minute: d_minute } = departtime;
 					const { hour: a_hour, minute: a_minute } = arrivetime;
+					console.log(seats);
 					return (
 						<>
 							<Paper className={classes.paper} onClick={ () => handleSeatOpened(idx) }>
@@ -105,8 +167,58 @@ export default function Shift(props) {
 								<Typography variant="subtitle1" className={classes.paperText}>{`${a_hour}:${a_minute}`}</Typography>
 							</Paper>
 							<Collapse in={seatOpened[idx]} timeout="auto" unmountOnExit>
-								<Paper className={classes.seatPaper}>
-								</Paper>
+								<div className={classes.bigContainer}>
+									<IconButton aria-label="previos" disabled={currentIdx === 0} onClick={handlePreviousClick}>
+										<KeyboardArrowLeftIcon />
+									</IconButton>
+									<Paper className={classes.seatPaper}>
+										<div className={classes.carContainer}>
+											<div className={classes.car} ref={ref0}>
+												{ seats.slice(0, 40).map((val, idx) => {
+													const class_name = val ? classes.seat : classes.seatDisable;
+													return (
+														<div
+															className={class_name}
+														></div>
+													)
+												}) }
+											</div>
+											<div className={classes.car} ref={ref1}>
+												{ seats.slice(40, 80).map((val, idx) => {
+													const class_name = val ? classes.seat : classes.seatDisable;
+													return (
+														<div
+															className={class_name}
+														></div>
+													)
+												}) }
+											</div>
+											<div className={classes.car} ref={ref2}>
+												{ seats.slice(40, 80).map((val, idx) => {
+													const class_name = val ? classes.seat : classes.seatDisable;
+													return (
+														<div
+														className={class_name}
+													></div>
+													)
+												}) }
+											</div>
+											<div className={classes.car} ref={ref3}>
+												{ seats.slice(40, 80).map((val, idx) => {
+													const class_name = val ? classes.seat : classes.seatDisable;
+													return (
+														<div
+															className={class_name}
+														></div>
+													)
+												}) }
+											</div>
+										</div>
+									</Paper>
+									<IconButton aria-label="next" disabled={currentIdx === 3} onClick={handleNextClick}>
+										<KeyboardArrowRightIcon />
+									</IconButton>
+								</div>
 							</Collapse>
 						</>
 					);
